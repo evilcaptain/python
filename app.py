2d179fef97e9f9796e9d87135f5b447861c51668
evilcaptain <89057010+evilcaptain@users.noreply.github.com>
2022-09-12 16:02:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template('index.html', title='Docker Python', name='James')
+    return render_template('index.html', title='Docker Python 12-09-2022', name='Captain Lee')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
